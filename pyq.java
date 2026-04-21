edf2f67c1fb28310ddf5b60f600462ae702249a4
Pawan kumar <kumarpy5890@gmail.com>
2026-04-21 19:13:03 +0530

    Create pyq.java

---
+++ pyq.java
@@ -0,0 +1,16 @@
+public class pyq{
+    public static void main(String[]agrs){
+        int i,j,space;
+        space = 3;
+        for(i=1;i<=4;i++){
+            for(j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            space--;
+        }    
+    }
+}
